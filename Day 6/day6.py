c689ca89354e7b0c8f7f0cc5f110661a94644c6b
mahirhambiralovic <mahir.hambiralovic@gmail.com>
2019-12-08 15:28:31 +0100

    Working on day 6

--- Day 6/day6.py
+++ Day 6/day6.py
@@ -1,18 +1,46 @@
 from tree import Tree
 
+def consolidate(tree_list):
+    for tree1 in tree_list:
+        for tree2 in tree_list:
+            if tree1 == tree2:
+                continue
+            else:
+                if tree1.add_orbiter(tree2,tree2):
+                    tree_list.remove(tree2)
+                    print("REMOVING")
+                    print(len(tree_list))
+                    return
+
 orbits = open("input.txt","r").read().split()
-com = Tree()
-
-planet_list = []
+temp_trees = [Tree("COM")]
 
 for planets in orbits:
-    #orbitee_name = planet[:3]
+    orbitee_name = planets[:3]
     orbiter_name = planets[4:]
-    planet_list.append(Tree(orbiter_name))
-    #print(orbitee, orbiter)
+    added = False
+    # Check if we should att it as child to an existing tree
+    for tree in temp_trees:
+        if tree.contains(Tree(orbitee_name)):
+            added = tree.add_orbiter(Tree(orbitee_name), Tree(orbiter_name))
+            break
+    if not added:
+        child = Tree(orbiter_name)
+        t_tree = Tree(orbitee_name)
+        t_tree.add_child(child)
+        temp_trees.append(t_tree)
 
-for planets in orbits:
-    orbitee_name = planet[:3]
-    orbiter_name = planets[4:]
+for tree in temp_trees:
+    print(str(tree))
+print(len(temp_trees))
+print(len(orbits))
 
-    
+while len(temp_trees) > 1:
+    consolidate(temp_trees)
+print(str(temp_trees[0]))
+
+# MP5
+# 	B5J
+# 		81G
+# 			RDT
+# 				8GY
--- Day 6/tree.py
+++ Day 6/tree.py
@@ -13,17 +13,17 @@ class Tree:
         if len(self.children) == 0:
             return False
         for peeking_tree in self.children:
-            print(peeking_tree.name)
+            #print(peeking_tree.name)
             if peeking_tree.name == query_tree.name:
                 return True
             else:
-                return find(peeking_tree, query_tree)
+                return Tree.contains(peeking_tree, query_tree)
 
     def add_orbiter(self,query_tree,add_tree):
         if len(self.children) == 0:
             return False
         for peeking_tree in self.children:
-            print(peeking_tree.name)
+            #print(peeking_tree.name)
             # Found it and adding orbiter
             if peeking_tree.name == query_tree.name:
                 peeking_tree.add_child(add_tree)
@@ -38,7 +38,7 @@ class Tree:
         if len(self.children) == 0:
             return False
         for peeking_tree in self.children:
-            print(peeking_tree.name)
+            #print(peeking_tree.name)
             if peeking_tree.name == query_tree.name:
                 self.remove_child(query_tree)
                 add_tree.add_child(query_tree)
@@ -59,13 +59,18 @@ class Tree:
         for peeking_tree in self.children:
             peeking_tree.print()
 
-
-a = Tree("A")
-b = Tree("B")
-c = Tree("C")
-d = Tree("D")
-
-a.add_child(b)
-a.add_child(c)
-c.add_child(d)
-a.print()
+    def __str__(self, level=0):
+        ret = "\t"*level+self.name+"\n"
+        for child in self.children:
+            ret += child.__str__(level+1)
+        return ret
+#
+# a = Tree("A")
+# b = Tree("B")
+# c = Tree("C")
+# d = Tree("D")
+#
+# a.add_child(b)
+# a.add_child(c)
+# c.add_child(d)
+# a.print()
